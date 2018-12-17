7cc85af4f4d1f2873b59f2a6e1d63c350eeb5da3
Ivan Godorogea <igodorogea@gmail.com>
2018-12-17 22:20:16 +0200

    add express to proxy app

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,10 @@
 import * as functions from 'firebase-functions';
+import express from 'express';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+const app = express();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+app.get('/test', (req, res) => {
+  res.send(`${new Date().toUTCString()}`);
 });
+
+export const proxy = functions.https.onRequest(app);
