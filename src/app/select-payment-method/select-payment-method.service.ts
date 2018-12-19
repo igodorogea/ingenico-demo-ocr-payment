5e5d9b017c408ba22c7c10fa37ff464f0f717136
Ivan Godorogea <igodorogea@gmail.com>
2018-12-19 10:52:24 +0200

    add url parameters

--- src/app/select-payment-method/select-payment-method.service.ts
+++ src/app/select-payment-method/select-payment-method.service.ts
@@ -15,6 +15,12 @@ export class SelectPaymentMethodService {
 
   getOcrIframeUrl(): Observable<string> {
     return this.http.post<OcrUrl>('/muume/creditCard/ocr', null)
-      .pipe(map(resp => resp.addCreditCardUrl));
+      .pipe(map(resp => {
+        const returnUrl = new URL('/basket/success', window.location.href);
+        const url = new URL(resp.addCreditCardUrl, /^https?:\/\//i.test(resp.addCreditCardUrl) ? '' : window.location.href);
+        url.searchParams.set('is-ocr-payment', '1');
+        url.searchParams.set('return-url', returnUrl.href);
+        return url.href;
+      }));
   }
 }
